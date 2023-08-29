5ae5cea09b0683308316c37ee1b529ccd5a6bfc8
Yashraj <yashrajsinghmandloi@gmail.com>
2023-08-29 19:00:10 +0530

    first commit

---
+++ uploads/1692956497608-routes.js
@@ -0,0 +1,33 @@
+import http from 'http';
+
+const server = http.createServer((req,res)=>{
+    res.setHeader("Content-Type", "application/json");
+    switch (req.url) {
+        case "/books":
+            res.writeHead(200);
+            res.end(books);
+            break
+        case "/authors":
+            res.writeHead(200);
+            res.end(authors);
+            break
+        default:
+            res.writeHead(404);
+            res.end(JSON.stringify({error:"Resource not found"}));
+    }
+})
+server.listen(3000,()=>{
+    console.log("server listening on routes");
+})
+
+const books = JSON.stringify([
+    { title: "The Alchemist", author: "Paulo Coelho", year: 1988 },
+    { title: "The Prophet", author: "Kahlil Gibran", year: 1923 }
+]);
+
+const authors = JSON.stringify([
+    { name: "Paulo Coelho", countryOfBirth: "Brazil", yearOfBirth: 1947 },
+    { name: "Kahlil Gibran", countryOfBirth: "Lebanon", yearOfBirth: 1883 }
+]);
+
+
